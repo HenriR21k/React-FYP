78e693c230cd188641c42dbbc24fe404edabeb43
HenriR21k <k2017731@kingston.ac.uk>
2022-11-27 22:52:29 +0000

    added a logo

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -1,14 +1,17 @@
 import { NavLink } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './Sidebar.css';
-
+import logo from './Images/virtuteam.jpeg'
 
 function Sidebar() {
 
   const getLinkStyle = ({ isActive }) => ( isActive ? 'navSelected' : null);
   
   return (
+    
         <nav className='nav-menu'>
+          <NavLink to='/'><img className='navLogo' src={logo}/></NavLink>
+          
           <ul className='nav-menu-items'>
             {SidebarData.map((item, index) => {
               return (
--- src/components/layouts/SidebarData.js
+++ src/components/layouts/SidebarData.js
@@ -2,7 +2,10 @@ import React from "react";
 import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
 
+
+
 export const SidebarData = [
+  
   {
     title: "My Projects",
     path: "/",
